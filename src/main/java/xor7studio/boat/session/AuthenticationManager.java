732ed405769f349fae8e1f425008472bb25be2b1
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-05-07 14:29:53 +0800

    加入了Token的utils（未完成

--- src/main/java/xor7studio/boat/session/AuthenticationManager.java
+++ src/main/java/xor7studio/boat/session/AuthenticationManager.java
@@ -1,10 +1,15 @@
 package xor7studio.boat.session;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.Getter;
+import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -12,55 +17,71 @@ import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import java.time.Instant;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
 
 public class AuthenticationManager {
     public static final AuthenticationManager INSTANCE = new AuthenticationManager();
+    private final Path keyPairFile=Path.of("AUTHENTICATION_KEY_PAIR");
+    //移动到Settings
+    @Getter
+    @Setter
+    private static String ISSUER = "Boat";
     protected AuthenticationManager(){
         try{
-            Path keyPairFile=Path.of("AUTHENTICATION_KEY_PAIR");
-            if (Files.exists(keyPairFile)) {
-                authenticationKeyPair = loadKeyPairFromFile(keyPairFile);
-            } else {
-                authenticationKeyPair = generateKeyPair();
-                saveKeyPairToFile(keyPairFile, authenticationKeyPair);
-            }
+            authenticationKeyPair = Files.exists(keyPairFile)?
+                    loadKeyPairFromFile(keyPairFile):generateKeyPair();
         } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
             throw new RuntimeException(e);
         }
     }
-    @Getter
     private final KeyPair authenticationKeyPair;
-    private static @NotNull KeyPair loadKeyPairFromFile(Path keyPairFile) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
-        byte[] bytes = Files.readAllBytes(keyPairFile);
-        String content = new String(bytes, StandardCharsets.UTF_8);
+    public PublicKey getAuthenticationPublicKey(){
+        return authenticationKeyPair.getPublic();
+    }
+    public String generateBearerToken(String data,long expirationTime, TemporalUnit unit){
+        Instant now = Instant.now();
+        Instant expiration = now.plus(expirationTime, unit);
+        return Jwts.builder()
+                .setSubject(data)
+                .setIssuer("Boat")
+                .setAudience("Authentication")
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(expiration))
+                .signWith(authenticationKeyPair.getPrivate(), SignatureAlgorithm.RS256)
+                .compact();
+    }
+    public String readBearerToken(String token) throws SignatureException,ExpiredJwtException {
+        PublicKey publicKey = authenticationKeyPair.getPublic();
+        Jws<Claims> claimsJws = Jwts.parserBuilder().setSigningKey(publicKey).build().parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        if(claims.getExpiration().before(Date.from(Instant.now())))
+            throw new ExpiredJwtException(claimsJws.getHeader(), claims,"");
+        if(claims.getIssuer().equals(""))
+        return claims.getSubject();
+    }
+    private @NotNull KeyPair loadKeyPairFromFile(Path keyPairFile) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        String content = Files.readString(keyPairFile);
         String[] parts = content.split(":");
-
-        byte[] publicKeyBytes = Base64.getDecoder().decode(parts[0]);
-        byte[] privateKeyBytes = Base64.getDecoder().decode(parts[1]);
-
+        if(parts.length!=2) return generateKeyPair();
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Decoders.BASE64.decode(parts[0]));
         PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
-
-        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(Decoders.BASE64.decode(parts[1]));
         PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
-
         return new KeyPair(publicKey, privateKey);
     }
 
-    private static void saveKeyPairToFile(Path keyPairFile, @NotNull KeyPair keyPair) throws IOException {
-        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
-        byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
-
-        String content = Base64.getEncoder().encodeToString(publicKeyBytes) + ":" + Base64.getEncoder().encodeToString(privateKeyBytes);
-        Files.writeString(keyPairFile, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    private void saveKeyPairToFile(@NotNull KeyPair keyPair) throws IOException {
+        String publicKey = Encoders.BASE64.encode(keyPair.getPublic().getEncoded());
+        String privateKey = Encoders.BASE64.encode(keyPair.getPrivate().getEncoded());
+        Files.writeString(keyPairFile, publicKey+":"+privateKey, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
-    private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(2048);
-        return keyPairGenerator.generateKeyPair();
+    private KeyPair generateKeyPair() throws NoSuchAlgorithmException, IOException {
+        KeyPair result=Keys.keyPairFor(SignatureAlgorithm.RS256);
+        saveKeyPairToFile(result);
+        return result;
     }
 }
