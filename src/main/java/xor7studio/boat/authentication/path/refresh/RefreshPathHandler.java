c16590e6daa104ec025fe6b6db3811b56d26cfc0
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-06-01 20:04:32 +0800

    Update RefreshPathHandler.java

--- src/main/java/xor7studio/boat/authentication/path/refresh/RefreshPathHandler.java
+++ src/main/java/xor7studio/boat/authentication/path/refresh/RefreshPathHandler.java
@@ -1,13 +1,31 @@
 package xor7studio.boat.authentication.path.refresh;
 
+import xor7studio.boat.GsonUtils;
+import xor7studio.boat.authentication.AuthenticationUtils;
 import xor7studio.boat.authentication.path.PathHandler;
 import xor7studio.boat.authentication.path.PathHandlerResult;
 import xor7studio.boat.authentication.path.PathRequestData;
+import xor7studio.boat.authentication.path.signin.SignInRequestData;
+import xor7studio.boat.authentication.path.signin.SignInResponseData;
+
+import java.nio.charset.StandardCharsets;
+import java.time.temporal.ChronoUnit;
 
 public class RefreshPathHandler extends PathHandler {
     @Override
-    public PathHandlerResult parse(PathRequestData request) {
-
-        return null;
+    public PathHandlerResult parse(PathRequestData request){
+        RefreshRequestData requestData =
+                GsonUtils.getGsonInstance().fromJson(
+                        request.getHttpRequest()
+                                .content()
+                                .toString(StandardCharsets.UTF_8),
+                        RefreshRequestData.class);
+        RefreshResponseData responseData = new RefreshResponseData();
+        responseData.token = AuthenticationUtils.INSTANCE.generateBearerToken(
+                request.getTokenSubject(),
+                requestData.expire_in,
+                ChronoUnit.DAYS);
+        return PathHandlerResult.builder()
+                .body(AuthenticationUtils.INSTANCE.generateBearerToken()).build();
     }
 }
